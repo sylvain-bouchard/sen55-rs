1e318e000063305801001c4a850d66c9f2df18f3
Sylvain Bouchard <sylvain.bouchard@live.ca>
2026-08-02 10:28:17 -0400

    refactor: remove dead code

--- sen5x-c-wrapper/src/lib.rs
+++ sen5x-c-wrapper/src/lib.rs
@@ -2,26 +2,6 @@
 
 mod ffi;
 
-// Ensure we link the static C library when building our core crate
-#[link(name = "sensirion_c", kind = "static")]
-extern "C" {
-    // Exact signatures from the compiled Sensirion headers
-    fn sen5x_device_reset() -> i16;
-    fn sen5x_read_data_ready(data_ready: &mut bool) -> i16;
-    fn sen5x_start_measurement() -> i16;
-    fn sen5x_stop_measurement() -> i16;
-    fn sen5x_read_measured_values(
-        mass_concentration_pm1p0: &mut u16,
-        mass_concentration_pm2p5: &mut u16,
-        mass_concentration_pm4p0: &mut u16,
-        mass_concentration_pm10p0: &mut u16,
-        ambient_humidity: &mut i16,
-        ambient_temperature: &mut i16,
-        voc_index: &mut i16,
-        nox_index: &mut i16,
-    ) -> i16;
-}
-
 /// Custom error type wrapping the raw C status return values
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum Sen5xError {
